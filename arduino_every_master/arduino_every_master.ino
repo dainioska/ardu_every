3dde61d4d5ab9c1a1c8ff0b7c59774cffdd45971
dainioshka <dainioshka@gmail.com>
2020-04-15 10:46:36 +0300

    add delay>> arduino_every_master.ino

--- arduino_every_master/arduino_every_master.ino
+++ arduino_every_master/arduino_every_master.ino
@@ -2,12 +2,25 @@
 
 #include <Wire.h>
 
+//Array of patterns  version "hex"
+int datArray[20]={0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
+
 void setup() {
   Serial.begin(9600);
   Wire.begin();
-  
+  delay(10000);   //delay 10sek. 
 }
 
 void loop() {
 
+for(int i =0; i<20; i++)     //init parallel sequence of patterns from array at chip PCAL6524
+{  
+Wire.beginTransmission(0x40);  //chip PCAL6524 I2C address 
+Wire.write(datArray[i]);
+Wire.endTransmission();
+delay(3000);
+Serial.print("Pattern FIRST: ");
+Serial.println(i);
+}
+//delay(60000);
 }
